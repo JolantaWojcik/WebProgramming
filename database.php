6e6f51ad2c4488e22dd99f0c426146ff1e507170
KatarzynaPiazdecka <katarzynaPiazdecka@gmail.com>
2015-07-14 22:50:38 +0200

    Update database.php

--- database.php
+++ database.php
@@ -10,27 +10,32 @@ class Database
     
     private function __construct()
     {
-        try 
-        {
+        try //robimy try..catch blok do przechwytywania wyjatkow
+        //po Try dajemy kod ktory moze sie nie powiesc. Gdy kod wyrzuci wyjatek, przejmuje go blok Catch
+        { 
 	//$con=pg_connect("host=sbazy user=s175371 dbname=s175371 password=9KMgKf6t"); 	
 	//('pgsql:host=localhost;dbname=DBNAME', 'USERNAME', 'PASSWORD');
 	
-            $this->_pdo = new PDO('pgsql:host=sbazy; dbname=s175371', 's175371', '9KMgKf6t');
+            $this->_pdo = new PDO('pgsql:host=sbazy; dbname=s175371', 's175371', '9KMgKf6t'); // This jest referencja do danego obiektu
 		//('mysql:host=localhost; dbname=dogs', 'root', 'piaskowa123',
                 //array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-            $this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //PDO - PHP Data Objects class. Attr_errmode - error reporting.  
+            									//Errmode_exception - setting error code and thoriwng PDOException
+            									// and set its properties to reflect the error code and error info
         }
         catch(PDOException $e) 
         {
-            die($e->getMessage());
+            die($e->getMessage()); //die - wysiwetla komunikat i konczy wykonanie skryptu gdy zwrocona wartosc jest False
+            			   // gets die PDOException Message
         }
     }
     
-    public function getInstance()
+    public function getInstance() // getInstance jest z Singelton pattern
+    				// used for instantiate no more than one instance of a class
     {
-        if(!isset(self::$_instance))
-        {
-            self::$_instance = new Database();
+        if(!isset(self::$_instance)) //isset - determine if variable is set and is not null
+        { // :: wywoluje metody klas bez tworzenia instancji obiektow (statyczne wywolanie metody)
+            self::$_instance = new Database(); // wywolujemy instance i przypisujemy 
         }
         return self::$_instance;
     }
